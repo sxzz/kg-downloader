657253881e2b147a9b89253f6ff39f20af49ae10
三咲智子 <sxzz@sxzz.moe>
2022-06-18 00:09:47 +0800

    test: fix download path

--- tests/downloader.test.ts
+++ tests/downloader.test.ts
@@ -1,13 +1,17 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { describe, expect, it } from 'vitest'
 import { downloadSong, getSongInfo } from '../src'
 
 describe('downloader should work', () => {
+  const dirname = path.dirname(fileURLToPath(import.meta.url))
+
   it('getSongInfo should work', async () => {
     const info = await getSongInfo('YFq3ycYfBeVVPYji')
     expect(info).toBeTruthy()
   })
 
   it('downloadSong should work', async () => {
-    await downloadSong('YFq3ycYfBeVVPYji', './downloads')
+    await downloadSong('YFq3ycYfBeVVPYji', path.resolve(dirname, 'temp'))
   })
 })
