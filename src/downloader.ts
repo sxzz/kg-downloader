73319c797b85cacb6167c0ed3855d8c991294ba2
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-08-12 22:53:21 +0800

    chore: fix type error

--- src/downloader.ts
+++ src/downloader.ts
@@ -2,18 +2,27 @@ import { Buffer } from 'node:buffer'
 import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 
-export const filterNick = (nick: string) =>
-  nick.replaceAll(/\[em\](.*?)\[\/em\]/g, '').replaceAll(/["*/:<>?\\|]/g, '')
+export function filterNick(nick: string): string {
+  return nick
+    .replaceAll(/\[em\](.*?)\[\/em\]/g, '')
+    .replaceAll(/["*/:<>?\\|]/g, '')
+}
 
-export const getSongInfo = async (shareId: string) => {
+export async function getSongInfo(shareId: string): Promise<any> {
   const url = `https://node.kg.qq.com/play?s=${shareId}`
   const text = await fetch(url).then((r) => r.text())
   const regex = /window\.__DATA__ = (.*?); <\/script>/
   const m = regex.exec(text)
+  if (!m) {
+    throw new Error('Failed to extract song info')
+  }
   return JSON.parse(m[1])
 }
 
-export const downloadSong = async (shareId: string, outputPath: string) => {
+export async function downloadSong(
+  shareId: string,
+  outputPath: string,
+): Promise<void> {
   const info = await getSongInfo(shareId)
   const url = info.detail.playurl
   if (!url) {
@@ -26,17 +35,18 @@ export const downloadSong = async (shareId: string, outputPath: string) => {
     method: 'GET',
   }).then((res) => res.arrayBuffer())
 
-  const filename = `${filterNick(info.detail.nick)} - ${
-    info.detail.song_name
-  }.mp3`
+  const filename = `${filterNick(info.detail.nick)} - ${info.detail.song_name}.mp3`
   const dest = path.resolve(outputPath, filename)
   await writeFile(dest, Buffer.from(data))
 }
 
-export const getUserInfo = async (uid: string) => {
+export async function getUserInfo(uid: string): Promise<any> {
   const url = `https://node.kg.qq.com/personal?uid=${uid}`
   const text = await fetch(url).then((r) => r.text())
   const regex = /window\.__DATA__ = (.*?); <\/script>/
   const m = regex.exec(text)
+  if (!m) {
+    throw new Error('Failed to extract user info')
+  }
   return JSON.parse(m[1])
 }
